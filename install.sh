9cb2f19ebcff4525292090fdb98854ae46e0b820
Xarvalus <GromadzkiMarcin@gmail.com>
2017-12-21 16:29:27 +0100

    initial

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+command=${1}
+
+# docker run -it dbpediaspotlight/lucene-quickstarter bash
+if [[ $command == "build" ]];then
+    docker-compose build
+elif [[ $command == "up" ]];then
+    docker-compose up
+elif [[ $command == "down" ]];then
+    docker-compose down 
+elif [[ $command == "lucyna" ]];then
+    docker exec -it lucyna_dbpedia_1 bash   
+fi
